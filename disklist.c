1b16d7c5ae910b14be44b97f8c61ed6a927bb3a1
Stella Zarei <zareistella@gmail.com>
2023-03-31 16:32:26 -0700

    end of part 2

--- disklist.c
+++ disklist.c
@@ -20,11 +20,15 @@
 
 typedef struct {
     char    filename[9];
-    char    extension[3];
+    char    extension[4];
     char    attr;
+    int     creationMin;
+    int     creationHour;
+    int     creationDay;
+    int     creationMonth;
+    int     creationYear;
     uint32_t    size;
-    uint8_t     createTimeMicroSec;
-    uint16_t    createTime;
+    
     
 }__attribute__ ((packed)) dirEntry_t;
 
@@ -36,13 +40,39 @@ void extractEntry(dirEntry_t* entryPtr, char* dirPtr){
         entryPtr->filename[b] = (char)dirPtr[b];
     }
     entryPtr->filename[b] = '\0';
+    for(b = 0; b < 3; b++){
+        entryPtr->extension[b] = (char)dirPtr[8+b];
+    }
+    entryPtr->extension[b] = '\0';
+    if (dirPtr[11] == 0x10){
+        entryPtr->attr = 'D';
+    }else{
+        entryPtr->attr = 'F';
+
+        entryPtr->creationYear = (((dirPtr[17] & 0xfe)) >> 1) + 1980;
+		entryPtr->creationMonth = ((dirPtr[16] & 0xe0) >> 5) + (((dirPtr[17] & 0x01)) << 3);
+		entryPtr->creationDay = (dirPtr[16] & 0x1f);
+		entryPtr->creationHour = (dirPtr[15] & 0xf8) >> 3;
+		entryPtr->creationMin = ((dirPtr[14] & 0xe0) >> 5) + ((dirPtr[15] & 0x7) << 3);
+        
+        entryPtr->size = (dirPtr[28] & 0xFF) + ((dirPtr[29] & 0xFF) << 8) + ((dirPtr[30] & 0xFF) << 16) + ((dirPtr[31] & 0xFF) << 24);;
+    }
+
 }
 
 void printDirectoryContents(char* parentName, dirEntry_t* entryPtr, int count){
-    printf("Directory: %s\n", parentName);
-    printf("=========================\n");
+    printf("%s\n", parentName);
+    printf("====================\n");
     for(int i = 0; i < count; i++){
-        printf("\t%s\n", (entryPtr[i]).filename);
+        if(entryPtr[i].attr == 'D'){
+            printf("\tD            %s\n", entryPtr[i].filename);
+        }
+        else{
+            printf("\tF %10d %s.%s\t%02d-%02d-%02d %02d:%02d\n", 
+                entryPtr[i].size, entryPtr[i].filename, entryPtr[i].extension, 
+                entryPtr[i].creationDay, entryPtr[i].creationMonth, entryPtr[i].creationYear,
+                entryPtr[i].creationHour, entryPtr[i].creationMin ); 
+        }
     }
 }
 
@@ -58,12 +88,10 @@ void traverseDirectory(char* parentName, char* fileptr, int physicalSector) {
 		}
         if((dir[11] & 0x10) && dir[0] != 0x2e){
             int logicalCluster = dir[26] + (dir[27] << 8);
-            traverseDirectory(&dirContents[count-1].filename, fileptr, logicalCluster+31);   
-		}
-        
+            traverseDirectory(dirContents[count-1].filename, fileptr, logicalCluster+31);   
+		} 
         dir+=32;
 	}
-
     printDirectoryContents(parentName, dirContents, count);
 }
 
@@ -97,7 +125,7 @@ int main(int argc, char* argv[]){
         return FAILED_EXIT;
     }
 
-    char rootDir[] = "Root Directory";
+    char* rootDir = "Root Directory";
     // work with the disk
     traverseDirectory(rootDir, fileptr, 19);
 
